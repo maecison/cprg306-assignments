281c256a1ed339ee1e5428c1848e96e979a2a464
maecison <mae.ison@outlook.com>
2023-09-14 16:27:14 -0600

    Updated header to "My Shopping List"

--- app/week2/page.js
+++ app/week2/page.js
@@ -4,7 +4,7 @@ import EventList from "./event-list";
 export default function Page() {
     return (
         <main>
-            <h1>Week 2</h1>
+            <h1>My Shopping List</h1>
             <Link href="/">Back to Home</Link>
             <EventList />
         </main>
