1b8d5503d1af4416b5ae000c1be48c4ac6e84140
maecison <mae.ison@outlook.com>
2023-09-14 16:31:28 -0600

    Changed to Student Info

---
+++ app/week2/student-info.js
@@ -0,0 +1,9 @@
+export default function EventList(){
+    return (
+        <div>
+            <p>Name: Mae Ison</p>
+            <p>Course section: CPRG 306 C</p>
+            <p>https://github.com/maecison</p>
+        </div>
+    )
+}
